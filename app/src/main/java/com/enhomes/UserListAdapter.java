b41ce958f51bc8864afb826ee82e186d4c8577ae
Pankti002 <panktishah8902@gmail.com>
2023-03-29 22:40:34 +0530

    tableView

--- app/src/main/java/com/enhomes/UserListAdapter.java
+++ app/src/main/java/com/enhomes/UserListAdapter.java
@@ -38,15 +38,7 @@ public class UserListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater layoutInflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view=layoutInflater.inflate(R.layout.table_example,null);
-
-
-//        TextView tvData = view.findViewById(R.id.tv_data);
-//        tvData.setText(userLangModelArrayList.get(position).getRoleId()+" "+userLangModelArrayList.get(position).get_id()+" "+
-//                userLangModelArrayList.get(position).getFirstName()+" "+userLangModelArrayList.get(position).getLastName()+" "
-//                +userLangModelArrayList.get(position).getDateOfBirth()+" "+userLangModelArrayList.get(position).getAge()+" "+
-//                userLangModelArrayList.get(position).getGender()+" "+userLangModelArrayList.get(position).getContactNo()+
-//                " "+userLangModelArrayList.get(position).getEmail()+" "+userLangModelArrayList.get(position).getPassword());
+        view=layoutInflater.inflate(R.layout.user_table,null);
 
         TextView tvRoleName,tvFn,tvLn,tvDob,tvAge,tvGen,tvCnt,tvEm;
 
@@ -103,6 +95,15 @@ public class UserListAdapter extends BaseAdapter {
 
                 Intent intent=new Intent(context,UserUpdateActivity.class);
                 intent.putExtra("USER_ID",id);
+                intent.putExtra("ROLE_ID",userLangModelArrayList.get(position).getRoleId());
+                intent.putExtra("FIRST_NAME",userLangModelArrayList.get(position).getFirstName());
+                intent.putExtra("LAST_NAME",userLangModelArrayList.get(position).getLastName());
+                intent.putExtra("DATE_OF_BIRTH",userLangModelArrayList.get(position).getDateOfBirth());
+                intent.putExtra("AGE",userLangModelArrayList.get(position).getAge());
+                intent.putExtra("GENDER",userLangModelArrayList.get(position).getGender());
+                intent.putExtra("CONTACT_NO",userLangModelArrayList.get(position).getContactNo());
+                intent.putExtra("EMAIL",userLangModelArrayList.get(position).getEmail());
+                intent.putExtra("PASSWORD",userLangModelArrayList.get(position).getPassword());
 
                 context.startActivity(intent);
             }
